3724a861e5448826c0df0fa9e366c0d9c7ac4bf7
Abhimanyu Kumar Vatsa <itorian@live.in>
2015-12-04 11:33:09 +0530

    init

---
+++ EmptyWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using EmptyWebAPI.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace EmptyWebAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // Data source
+        List<Student> db = new List<Student>();
+        public StudentController()
+        {
+            db = new List<Student>
+            {
+                new Student { Id = 1, Name = "Abhimanyu K Vatsa", City = "Bokaro" },
+                new Student { Id = 2, Name = "Deepak Kumar Gupta", City = "Bokaro" },
+                new Student { Id = 3, Name = "Manish Kumar", City = "Muzaffarpur" },
+                new Student { Id = 4, Name = "Rohit Ranjan", City = "Patna" },
+                new Student { Id = 5, Name = "Shiv", City = "Motihari" },
+                new Student { Id = 6, Name = "Rajesh Singh", City = "Dhanbad" },
+                new Student { Id = 7, Name = "Staya", City = "Bokaro" },
+                new Student { Id = 8, Name = "Samiran", City = "Chas" },
+                new Student { Id = 9, Name = "Rajnish", City = "Bokaro" },
+                new Student { Id = 10, Name = "Mahtab", City = "Dhanbad" }
+            };
+        }
+
+        public IHttpActionResult Get(string sort = "id", int page = 1, int pageSize = 5)
+        {
+            return Ok(db);
+        }
+
+    }
+}
